a9ff7e0d937d94d0f26f75357d89bfc124cbfffe
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2015-03-27 15:23:23 +0800

    Prototype 2.3.1 -Post Page Animations-
    - Added spinning submitButton animation on document.ready();
    - FadeIn, FadeOut transitions in-between tabs

--- WEBAPDE/post.php
+++ WEBAPDE/post.php
@@ -36,17 +36,26 @@ else{
 						}
   					});
   					$(document).ready(function(){
+  						$(".menuBox").animate({opacity: 1, top: "15%"}, 500);
+  						$(".submitButton").addClass('spinButton');
 						$('ul.tabs li').click(function(){
 							var tab_id = $(this).attr('data-tab');
 							$('ul.tabs li').removeClass('current');
 							$('.tab-content').removeClass('current');
 							$(this).addClass('current');
 							$("#"+tab_id).addClass('current');
-						})
+							if(tab_id == "postReview"){
+								$('.submitButton').css('-webkit-animation-iteration-count', '0');
+								$('#inputData1').css('opacity', '0');
+								$('#inputData1').animate({opacity: 1}, "slow");
+							}
+							else if(tab_id == "postRecipe"){
+								$('.submitButton').css('-webkit-animation-iteration-count', '0');
+								$('#inputData2').css('opacity', '0');
+								$('#inputData2').animate({opacity: 1}, "slow");
+							}
+						});
 					});
-					$(document).ready(function() {
-    					$(".menuBox").animate({opacity: 1, top: "15%"}, 500);
-  					});
 				</script>
 				<a href = "home.php" class ="no"><p class = "headName">potato.</p></a>
 				<input type = "search" id = "searchBar">
@@ -94,31 +103,33 @@ else{
 				</ul>
 				<div id = "postReview" class = "tab-content current" align = "center">
 					<form action ="post_review.php" method="POST" enctype="multipart/form-data">
-						<br>
-						<input type = "text" placeholder = "Title" class = "pBox" id = "title" name="review_title"/>
-						<br>
-						<br>
-						<textarea placeholder = "Insert text here" class = "tBox"cols = "10" rows = "50 "id = "textBody" name="review_txt"/></textarea>
-						<br>
-						<br>
-						&nbsp;&nbsp;&nbsp;&nbsp;
-						&nbsp;&nbsp;&nbsp;&nbsp;
-						&nbsp;&nbsp;&nbsp;&nbsp;
-						<input type = "file" name = "fileToUpload">
-						<br>
-						<br>
-						<span class = "rating">
-					        <input type = "radio" class = "rating-input" id = "rating-input-1-5" value ="5" name = "rating-input-1">
-					        <label for = "rating-input-1-5" class = "rating-star"></label>
-					        <input type = "radio" class = "rating-input" id = "rating-input-1-4" value="4" name = "rating-input-1">
-					        <label for = "rating-input-1-4" class = "rating-star"></label>
-					        <input type = "radio" class="rating-input" id = "rating-input-1-3" value="3"name = "rating-input-1">
-					        <label for = "rating-input-1-3" class = "rating-star"></label>
-					        <input type = "radio" class="rating-input" id = "rating-input-1-2" value ="2" name = "rating-input-1">
-					        <label for = "rating-input-1-2" class = "rating-star"></label>
-					        <input type = "radio" class = "rating-input" id = "rating-input-1-1" value ="1" name = "rating-input-1">
-					        <label for = "rating-input-1-1" class = "rating-star"></label>
-						</span>
+						<div id = "inputData1">
+							<br>
+							<input type = "text" placeholder = "Title" class = "pBox" id = "title" name="review_title"/>
+							<br>
+							<br>
+							<textarea placeholder = "Insert text here" class = "tBox"cols = "10" rows = "50 "id = "textBody" name="review_txt"/></textarea>
+							<br>
+							<br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+							&nbsp;&nbsp;&nbsp;&nbsp;
+							&nbsp;&nbsp;&nbsp;&nbsp;
+							<input type = "file" name = "fileToUpload">
+							<br>
+							<br>
+							<span class = "rating">
+						        <input type = "radio" class = "rating-input" id = "rating-input-1-5" value ="5" name = "rating-input-1">
+						        <label for = "rating-input-1-5" class = "rating-star"></label>
+						        <input type = "radio" class = "rating-input" id = "rating-input-1-4" value="4" name = "rating-input-1">
+						        <label for = "rating-input-1-4" class = "rating-star"></label>
+						        <input type = "radio" class="rating-input" id = "rating-input-1-3" value="3"name = "rating-input-1">
+						        <label for = "rating-input-1-3" class = "rating-star"></label>
+						        <input type = "radio" class="rating-input" id = "rating-input-1-2" value ="2" name = "rating-input-1">
+						        <label for = "rating-input-1-2" class = "rating-star"></label>
+						        <input type = "radio" class = "rating-input" id = "rating-input-1-1" value ="1" name = "rating-input-1">
+						        <label for = "rating-input-1-1" class = "rating-star"></label>
+							</span>
+						</div>
 					    <div class = "submitButton">
 							<button id = "postButton" onclick = "document.forms["pReview"].submit();"><img class = "postImg" src = "images/checkButton.png"></button>
 						</div>
@@ -126,23 +137,25 @@ else{
 				</div>					
 				<div id = "postRecipe" class = "tab-content" align = "center">
 					<form action ="post_recipe.php" method="POST" enctype = "multipart/form-data">
-						<br>
-						<input type = "text" placeholder = "Title" class = "pBox" id = "title" name="recipe_title"/>
-						<br>
-						<br>
-						<textarea placeholder = "Ingredients" class = "reBox"cols = "10" rows = "50 "id = "ingredientsText" name="ingredients_txt"/></textarea>
-						<br>
-						<br>
-						<textarea placeholder = "Directions" class = "reBox"cols = "10" rows = "50 "id = "directionsText" name="directions_txt"/></textarea>
-						<br>
-						<br>
-						<textarea placeholder = "Nutrition Facts" class = "reBox"cols = "10" rows = "50 "id = "nutritionsText" name="facts_txt"/></textarea>
-						<br>
-						<br>
-						&nbsp;&nbsp;&nbsp;&nbsp;
-						&nbsp;&nbsp;&nbsp;&nbsp;
-						&nbsp;&nbsp;&nbsp;&nbsp;
-						<input type = "file" name = "fileToUpload">
+						<div id = "inputData2">
+							<br>
+							<input type = "text" placeholder = "Title" class = "pBox" id = "title" name="recipe_title"/>
+							<br>
+							<br>
+							<textarea placeholder = "Ingredients" class = "reBox"cols = "10" rows = "50 "id = "ingredientsText" name="ingredients_txt"/></textarea>
+							<br>
+							<br>
+							<textarea placeholder = "Directions" class = "reBox"cols = "10" rows = "50 "id = "directionsText" name="directions_txt"/></textarea>
+							<br>
+							<br>
+							<textarea placeholder = "Nutrition Facts" class = "reBox"cols = "10" rows = "50 "id = "nutritionsText" name="facts_txt"/></textarea>
+							<br>
+							<br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+							&nbsp;&nbsp;&nbsp;&nbsp;
+							&nbsp;&nbsp;&nbsp;&nbsp;
+							<input type = "file" name = "fileToUpload">
+						</div>
 						<div class = "submitButton">
 							<button id = "postButton" onclick = "document.forms["pRecipe"].submit();"><img class = "postImg" src = "images/checkButton.png"></button>
 						</div>
